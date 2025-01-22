2478909d8745486d9eebcbe130102cb63abd7a83
star-pooh <eunki92@gmail.com>
2025-01-22 19:34:02 +0900

    🚑 Fix : Lv2-6 JPA Cascade - 할 일 저장 시, 할 일을 생성한 유저가 담당자로 등록되도록 cascade 수정

--- src/main/java/org/example/expert/domain/todo/entity/Todo.java
+++ src/main/java/org/example/expert/domain/todo/entity/Todo.java
@@ -17,7 +17,8 @@ import java.util.List;
 @Table(name = "todos")
 public class Todo extends Timestamped {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String contents;
@@ -30,7 +31,7 @@ public class Todo extends Timestamped {
     @OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE)
     private List<Comment> comments = new ArrayList<>();
 
-    @OneToMany(mappedBy = "todo")
+    @OneToMany(mappedBy = "todo", cascade = CascadeType.PERSIST)
     private List<Manager> managers = new ArrayList<>();
 
     public Todo(String title, String contents, String weather, User user) {
